c2f1f34c276ac69ac24635e545e80355b5b82ff5
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-04 12:31:10 -0700

    Added Feature class to models

---
+++ DnDesigner/Models/Features.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DnDesigner.Models
+{
+    /// <summary>
+    /// Represents a character feature from various sources
+    /// i.e. Class, Subclass, Background
+    /// </summary>
+    public class Features
+    {
+        /// <summary>
+        /// The feature's Unique identifier
+        /// </summary>
+        [Key]
+        public int FeatureId { get; private set; }
+
+        /// <summary>
+        /// The feature's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The feature's source
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <summary>
+        /// The feature's description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The level the feature is available at
+        /// </summary>
+        public int Level { get; set; }
+    }
+}

e90d4d409d77faacb75803935dade40746978d68
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-04 12:51:22 -0700

    Added constructor to Features class

--- DnDesigner/Models/Features.cs
+++ DnDesigner/Models/Features.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace DnDesigner.Models
 {
@@ -33,5 +34,21 @@ namespace DnDesigner.Models
         /// The level the feature is available at
         /// </summary>
         public int Level { get; set; }
+
+
+        /// <summary>
+        /// Full constructor, sets all properties
+        /// </summary>
+        /// <param name="name">The name of the feature</param>
+        /// <param name="source">The source of the feature</param>
+        /// <param name="description">The feature's description</param>
+        /// <param name="level">The level the feature is available at</param>
+        public Features(string name, string source, string description, int level)
+        {
+            Name = name;
+            Source = source;
+            Description = description;
+            Level = level;
+        }
     }
 }

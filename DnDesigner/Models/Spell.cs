6be7a12a1d15e1124fbd04e244b1a3ad3a1f3c33
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-10-04 13:11:02 -0700

    Added spell documentation

--- DnDesigner/Models/Spell.cs
+++ DnDesigner/Models/Spell.cs
@@ -1,16 +1,93 @@
-﻿namespace DnDesigner.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DnDesigner.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Spell
     {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        [Key]
         public int SpellId { get; set; }
+
+        /// <summary>
+        /// The name of the spell
+        /// </summary>
         public string SpellName { get; set; }
+
+        /// <summary>
+        /// The base level of the spell
+        /// </summary>
         public int SpellLevel { get; set; }
+
+        /// <summary>
+        /// The school of magic the spell belongs to
+        /// </summary>
         public string SpellSchool { get; set; }
+
+        /// <summary>
+        /// The time it takes to cast the spell
+        /// Can be an action, bonus action, reaction, minutes, or hours
+        /// </summary>
         public string CastingTime { get; set; }
+
+        /// <summary>
+        /// The range of the spell
+        /// </summary>
         public string Range { get; set; }
+
+        /// <summary>
+        /// The components required to cast the spell
+        /// Can be verbal, somatic, material (with materials specified), or a combination of the three
+        /// </summary>
         public string Components { get; set; }
+
+        /// <summary>
+        /// How long the spell lasts
+        /// </summary>
         public string Duration { get; set; }
+
+        /// <summary>
+        /// The description of the spell
+        /// </summary>
         public string SpellDescription { get; set; }
-        public List<Spellcasting> Learnable { get; set; }
+
+        /// <summary>
+        /// Who can learn the spell
+        /// </summary>
+        public List<LearnableSpell> LearnedBy { get; set; }
+    }
+
+    /// <summary>
+    /// Shows which classes can learn which spells
+    /// </summary>
+    public class LearnableSpell
+    {
+        /// <summary>
+        /// A spell that can be learned by a class
+        /// </summary>
+        [ForeignKey("SpellId")]
+        public Spell Spell { get; set; }
+
+        /// <summary>
+        /// A class that can learn a spell
+        /// </summary>
+        [ForeignKey("SpellcastingId")]
+        public Spellcasting Spellcasting { get; set; }
+
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        /// <param name="spell">A spell that can be learned by a class</param>
+        /// <param name="spellcasting">A class that can learn a spell</param>
+        public LearnableSpell(Spell spell, Spellcasting spellcasting)
+        {
+            Spell = spell;
+            Spellcasting = spellcasting;
+        }
     }
 }

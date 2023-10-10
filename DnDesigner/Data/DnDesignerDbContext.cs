1a3c290a784591a8ced3aaabe0373b26baffe044
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-09 19:12:02 -0700

    Set all DbSets to public

--- DnDesigner/Data/DnDesignerDbContext.cs
+++ DnDesigner/Data/DnDesignerDbContext.cs
@@ -11,28 +11,28 @@ namespace DnDesigner.Data
         {
         }
 
-        DbSet<Proficiency> Proficiencies { get; set; }
-        DbSet<Background> Backgrounds { get; set; }
-        DbSet<BackgroundFeature> BackgroundFeatures { get; set; }
-        DbSet<BackgroundProficiency> BackgroundProficiencies { get; set; }
-        DbSet<Class> Classes { get; set; }
-        DbSet<ClassFeature> ClassFeatures { get; set; }
-        DbSet<ClassProficiency> ClassProficiencies { get; set; }
-        DbSet<Subclass> Subclasses { get; set; }
-        DbSet<SubclassFeature> SubclassFeatures { get; set; }
-        DbSet<Race> Races { get; set; }
-        DbSet<RaceFeature> RaceFeatures { get; set; }
-        DbSet<RaceProficiency> RaceProficiencies { get; set; }
-        DbSet<Spellcasting> Spellcasting { get; set; }
-        DbSet<Spell> Spells { get; set; }
-        DbSet<LearnableSpell> LearnableSpells { get; set; }
-        DbSet<Item> Items { get; set; }
-        DbSet<Character> Characters { get; set; }
-        DbSet<CharacterProficiency> CharacterProficiencies { get; set; }
-        DbSet<CharacterClass> CharacterClasses { get; set; }
-        DbSet<CharacterSpellcasting> CharacterSpellcasting { get; set;}
-        DbSet<KnownSpell> KnownSpells { get; set; }
-        DbSet<Inventory> Inventory { get; set; }
-        DbSet<InventoryItem> InventoryItems { get; set; } 
+        public DbSet<Proficiency> Proficiencies { get; set; }
+        public DbSet<Background> Backgrounds { get; set; }
+        public DbSet<BackgroundFeature> BackgroundFeatures { get; set; }
+        public DbSet<BackgroundProficiency> BackgroundProficiencies { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<ClassFeature> ClassFeatures { get; set; }
+        public DbSet<ClassProficiency> ClassProficiencies { get; set; }
+        public DbSet<Subclass> Subclasses { get; set; }
+        public DbSet<SubclassFeature> SubclassFeatures { get; set; }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<RaceFeature> RaceFeatures { get; set; }
+        public DbSet<RaceProficiency> RaceProficiencies { get; set; }
+        public DbSet<Spellcasting> Spellcasting { get; set; }
+        public DbSet<Spell> Spells { get; set; }
+        public DbSet<LearnableSpell> LearnableSpells { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<CharacterProficiency> CharacterProficiencies { get; set; }
+        public DbSet<CharacterClass> CharacterClasses { get; set; }
+        public DbSet<CharacterSpellcasting> CharacterSpellcasting { get; set;}
+        public DbSet<KnownSpell> KnownSpells { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; } 
     }
 }

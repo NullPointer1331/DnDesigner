605df621ab9e283f2016da1d6451e1d810b2ffb4
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-10 16:45:41 -0700

    Added Character controller

---
+++ DnDesigner/Controllers/CharactersController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DnDesigner.Data;
+using DnDesigner.Models;
+
+namespace DnDesigner.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private readonly DnDesignerDbContext _context;
+
+        public CharactersController(DnDesignerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Characters
+        public async Task<IActionResult> Index()
+        {
+              return _context.Characters != null ? 
+                          View(await _context.Characters.ToListAsync()) :
+                          Problem("Entity set 'DnDesignerDbContext.Characters'  is null.");
+        }
+
+        // GET: Characters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Characters == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters
+                .FirstOrDefaultAsync(m => m.CharacterId == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // GET: Characters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Characters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CharacterId,Name,Level,ProficiencyBonus,MaxHealth,CurrentHealth,TempHealth,AvailableHitDice,HitDieType,WalkingSpeed,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Resistances,Immunities,Vulnerabilities")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(character);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(character);
+        }
+
+        // GET: Characters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Characters == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters.FindAsync(id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Characters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CharacterId,Name,Level,ProficiencyBonus,MaxHealth,CurrentHealth,TempHealth,AvailableHitDice,HitDieType,WalkingSpeed,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Resistances,Immunities,Vulnerabilities")] Character character)
+        {
+            if (id != character.CharacterId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(character);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CharacterExists(character.CharacterId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(character);
+        }
+
+        // GET: Characters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Characters == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters
+                .FirstOrDefaultAsync(m => m.CharacterId == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // POST: Characters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Characters == null)
+            {
+                return Problem("Entity set 'DnDesignerDbContext.Characters'  is null.");
+            }
+            var character = await _context.Characters.FindAsync(id);
+            if (character != null)
+            {
+                _context.Characters.Remove(character);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CharacterExists(int id)
+        {
+          return (_context.Characters?.Any(e => e.CharacterId == id)).GetValueOrDefault();
+        }
+    }
+}

3b4a808e1dae654641e774dca653f3057c2db92a
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-10-11 11:58:02 -0700

    Added migration to add Race column to Character table

---
+++ DnDesigner/Data/Migrations/20231011185610_AddRaceToCharacterModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DnDesigner.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRaceToCharacterModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RaceId",
+                table: "Characters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_RaceId",
+                table: "Characters",
+                column: "RaceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Races_RaceId",
+                table: "Characters",
+                column: "RaceId",
+                principalTable: "Races",
+                principalColumn: "RaceId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Races_RaceId",
+                table: "Characters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Characters_RaceId",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "RaceId",
+                table: "Characters");
+        }
+    }
+}

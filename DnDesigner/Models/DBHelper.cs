e363c6178d1160cad8593dff28c56473646f2721
Elias McKee <eqbookfan@gmail.com>
2023-11-01 13:44:31 -0700

    Added Get and GetAll methods, and began documentation

---
+++ DnDesigner/Models/DBHelper.cs
@@ -0,0 +1,148 @@
+﻿namespace DnDesigner.Models
+{
+    public static class DBHelper
+    {
+        /* TODO:
+            Get methods -
+                * Backgrounds
+                * Character
+                * Class
+                * Feature
+                * Inventory
+                * Item
+                * Proficiency
+                * Race
+                * Spell
+                * Spellcasting
+                * Subclass
+         */
+
+        /// <summary>
+        /// Gets a <see cref="Background"/> from the database,
+        /// using the specified id.
+        /// </summary>
+        /// <param name="id">The id of the <see cref="Background"/> that you're looking for.</param>
+        /// <returns>The <see cref="Background"/> with the specified id.</returns>
+        public static Background GetBackground(int id)
+        {
+            return null;
+        }
+        
+        /// <summary>
+        /// Gets all of the <see cref="Background"/>s from the database.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="List{Background}"/> of all the <see cref="Background"/> objects in the database
+        /// </returns>
+        public static List<Background> GetAllBackgrounds() 
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Gets a <see cref="Character"/> from the database,
+        /// with the specified id.
+        /// </summary>
+        /// <param name="id">The id of the <see cref="Character"/> that you're looking for.</param>
+        /// <returns>The <see cref="Character"/> with the specified id.</returns>
+        public static Character GetCharacter(int id)
+        {
+            return null;
+        }
+
+        public static List<Character> GetAllCharacters()
+        {
+            return null;
+        }
+
+        public static Class GetClass(int id)
+        {
+            return null;
+        }
+
+        public static List<Class> GetAllClasses()
+        {
+            return null;
+        }
+
+        public static Feature GetFeature(int id)
+        {
+            return null;
+        }
+
+        public static List<Feature> GetAllFeatures()
+        {
+            return null;
+        }
+
+        public static Inventory GetInventory(int id)
+        {
+            return null;
+        }
+
+        public static List<Inventory> GetAllInventories()
+        {
+            return null;
+        }
+
+        public static Item GetItem(int id)
+        {
+            return null;
+        }
+
+        public static List<Item> GetAllItems()
+        {
+            return null;
+        }
+
+        public static Proficiency GetProficiency(int id)
+        {
+            return null;
+        }
+
+        public static List<Proficiency> GetAllProficiencies()
+        {
+            return null;
+        }
+
+        public static Race GetRace(int id)
+        {
+            return null;
+        }
+
+        public static List<Race> GetAllRaces()
+        {
+            return null;
+        }
+
+        public static Spell GetSpell(int id)
+        {
+            return null;
+        }
+
+        public static List<Spell> GetAllSpells()
+        {
+            return null;
+        }
+
+        public static Spellcasting GetSpellcasting(int id)
+        {
+            return null;
+        }
+
+        public static List<Spellcasting> GetAllSpellcastings()
+        {
+            return null;
+        }
+
+        public static Subclass GetSubclass(int id)
+        {
+            return null;
+        }
+
+        public static List<Subclass> GetAllSubclasses()
+        {
+            return null;
+        }
+    }
+}

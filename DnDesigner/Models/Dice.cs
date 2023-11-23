6379b7beb712ee4f7c9e6f126f6d6a5150dbb2ea
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-11-22 18:11:03 -0800

    Added documentation, adjusted strings for readability, and declared fields properly for their context.

--- DnDesigner/Models/Dice.cs
+++ DnDesigner/Models/Dice.cs
@@ -5,8 +5,6 @@
     /// </summary>
     static class Dice
     {
-        // Move all these methods into the Character class?!?!?!?!?
-
         #region properties
 
         /// <summary>
@@ -14,21 +12,40 @@
         /// </summary>
         static Random roll = new();
 
-        /*
-        public int D4 { get { return 5; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 4
+        /// </summary>
+        static int D4 = 5;
 
-        public int D6 { get { return 7; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 6
+        /// </summary>
+        static int D6 = 7;
 
-        public int D8 { get { return 9; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 8
+        /// </summary>
+        static int D8 = 9;
 
-        public int D10 { get { return 11; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 10
+        /// </summary>
+        static int D10 = 11;
 
-        public int D12 { get { return 13; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 12
+        /// </summary>
+        static int D12 = 13;
 
-        public int D20 { get { return 21; } }
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 20
+        /// </summary>
+        static int D20 = 21;
 
-        public int D100 { get { return 101; } }
-        */
+        /// <summary>
+        /// The upper limit to generate a random int from 1 to 100
+        /// </summary>
+        static int D100 = 101;
 
         #endregion
 
@@ -39,14 +56,50 @@
         /// <param name="dieSize">The maximum number on the die</param>
         /// <param name="dieCount">How many dice will be rolled</param>
         /// <returns>The sum of the rolls</returns>
-        static string RollBasic(int dieSize, int dieCount)
+        public static string RollBasic(int dieSize, int dieCount)
         {
-            int result = 0;
-            for (int i = 0; i < dieCount; i++)
+            int rollTotal = roll.Next(1, dieSize + 1);
+            string resultString = rollTotal.ToString();
+
+            // if die count is greater than 1, roll and concat string
+            for (int i = 1; i < dieCount; i++)
             {
-                result += roll.Next(1, dieSize + 1);
+                int currRoll = roll.Next(1, dieSize + 1);
+                rollTotal += currRoll;
+                resultString += " + " + currRoll.ToString();
             }
-            return result.ToString();
+            if (dieCount > 1)
+            {
+                resultString += " = " + rollTotal.ToString();
+            }
+            return resultString;
+        }
+
+        /// <summary>
+        /// Rolls a given size die a given number of times
+        /// </summary>
+        /// <param name="dieSize">The maximum number on the die</param>
+        /// <param name="dieCount">How many dice will be rolled</param>
+        /// <param name="modifier">The characters modifier for this roll</param>
+        /// <returns>The sum of the rolls</returns>
+        public static string RollBasic(int dieSize, int dieCount, int modifier)
+        {
+            int rollTotal = roll.Next(1, dieSize + 1);
+            string resultString = rollTotal.ToString();
+
+            // if die count is greater than 1, roll and concat string
+            for (int i = 1; i < dieCount; i++)
+            {
+                int currRoll = roll.Next(1, dieSize + 1);
+                rollTotal += currRoll;
+                resultString += " + " + currRoll.ToString();
+            }
+
+            // add on modifier
+            rollTotal += modifier;
+            resultString += " + " + modifier.ToString();
+            resultString += " = " + rollTotal.ToString();
+            return resultString;
         }
 
         /// <summary>
@@ -54,7 +107,7 @@
         /// </summary>
         /// <param name="roll">The roll to check</param>
         /// <returns>Natural 20 or the original value</returns>
-        static string CheckNat20(int roll)
+        public static string CheckNat20(int roll)
         {
             if (roll == 20)
             {
@@ -67,9 +120,9 @@
         /// Rolls one d20
         /// </summary>
         /// <returns>The result of the roll</returns>
-        static string RollSingleD20()
+        public static string RollSingleD20()
         {
-            int singleRoll = roll.Next(1, 21);
+            int singleRoll = roll.Next(1, D20);
             return CheckNat20(singleRoll);
         }
 
@@ -78,11 +131,11 @@
         /// </summary>
         /// <param name="modifier">The characters modifier for this roll</param>
         /// <returns>The result of the roll</returns>
-        static string RollSingleD20(int modifier)
+        public static string RollSingleD20(int modifier)
         {
-            int singleRoll = roll.Next(1, 21);
+            int singleRoll = roll.Next(1, D20);
             return CheckNat20(singleRoll) + " Modified by " + modifier.ToString() +
-                " for a total roll of " + (singleRoll + modifier).ToString();
+                " = " + (singleRoll + modifier).ToString();
         }
 
         /// <summary>
@@ -91,15 +144,18 @@
         /// <param name="rollType">True if roll is at advantage
         ///    , False if roll is at disadvantage</param>
         /// <returns>The result of the roll</returns>
-        static string RollAdvOrDis(bool rollType)
+        public static string RollAdvOrDis(bool rollType)
         {
-            int roll1 = roll.Next(1, 21);
-            int roll2 = roll.Next(1, 21);
+            int roll1 = roll.Next(1, D20);
+            int roll2 = roll.Next(1, D20);
+
+            // Advantage, take the higher roll
             if (rollType)
             {
                 return "You Keep A: " + CheckNat20(Math.Max(roll1, roll2)) +
                     ", Lower Roll: " + CheckNat20(Math.Min(roll1, roll2));
             }
+            // Disadvantage, take the lower roll
             return "You Keep A: " + CheckNat20(Math.Min(roll1, roll2)) +
                 ", Higher Roll: " + CheckNat20(Math.Max(roll1, roll2));
         }
@@ -111,20 +167,23 @@
         ///    , False if roll is at disadvantage</param>
         /// <param name="modifier">The characters modifier for this roll</param>
         /// <returns>The result of the roll</returns>
-        static string RollAdvOrDis(bool rollType, int modifier)
+        public static string RollAdvOrDis(bool rollType, int modifier)
         {
-            int roll1 = roll.Next(1, 21);
-            int roll2 = roll.Next(1, 21);
+            int roll1 = roll.Next(1, D20);
+            int roll2 = roll.Next(1, D20);
+
+            // Advantage, take the higher roll
             if (rollType)
             {
                 return "You Keep A: " + CheckNat20(Math.Max(roll1, roll2)) +
                         " Modified by " + modifier.ToString() +
-                        " for a total roll of " + (Math.Max(roll1, roll2) + modifier).ToString() +
+                        " = " + (Math.Max(roll1, roll2) + modifier).ToString() +
                     ", Lower Roll: " + CheckNat20(Math.Min(roll1, roll2));
             }
+            // Disadvantage, take the lower roll
             return "You Keep A: " + CheckNat20(Math.Min(roll1, roll2)) +
                         " Modified by " + modifier.ToString() +
-                        " for a total roll of " + (Math.Min(roll1, roll2) + modifier).ToString() +
+                        " = " + (Math.Min(roll1, roll2) + modifier).ToString() +
                 ", Higher Roll: " + CheckNat20(Math.Max(roll1, roll2));
         }
     }    

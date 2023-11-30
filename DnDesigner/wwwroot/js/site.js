e080615c986a2455d28dcc5b159494b06e37e6f3
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-11-30 14:44:20 -0800

    Moved toast code for trouble shooting

--- DnDesigner/wwwroot/js/site.js
+++ DnDesigner/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function assignToast(button, toast) {
+        let toastTrigger = document.getElementById(button)
+        let toastBox = document.getElementById(toast)
+
+        if (toastTrigger) {
+            let toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastBox)
+            toastBootstrap.show()
+        }
+}

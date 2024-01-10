8e6a5bd84c77da3ce8f205e46011d3f1ae5b6dca
Elias McKee <eqbookfan@gmail.com>
2024-01-10 13:16:36 -0800

    Made a new migration adding the Alignment property to the character table

--- DnDesigner/Data/Migrations/20240110211418_Added-Alignment.Designer.cs
+++ DnDesigner/Data/Migrations/20240110211418_Added-Alignment.Designer.cs
@@ -4,6 +4,7 @@ using DnDesigner.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DnDesigner.Data.Migrations
 {
     [DbContext(typeof(DnDesignerDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240110211418_Added-Alignment")]
+    partial class AddedAlignment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

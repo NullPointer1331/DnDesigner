95c843449839723a68fd64365a0898a1bf9e5983
Elias McKee <eqbookfan@gmail.com>
2024-02-12 15:56:04 -0800

    Added a verification token to the AspNetUsers table

---
+++ DnDesigner/Data/Migrations/20240212234600_AddedVerificationToken.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DnDesigner.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedVerificationToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+            name: "VerificationToken",
+            table: "AspNetUsers",
+            maxLength: 50,
+            nullable: true,
+            defaultValueSql: "NEWID()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+            name: "VerificationToken",
+            table: "AspNetUsers");
+        }
+    }
+}
